fa642ca820ffe148e9ff0dc6a3250bb814a7ad65
Klaus Jackson <klausjackson2@gmail.com>
2024-04-23 01:11:12 +0700

    Update main.py

--- Modern Version/main.py
+++ Modern Version/main.py
@@ -5,6 +5,28 @@ from Custom_Widgets.QAppSettings import QAppSettings
 from src.functions import Func
 
 
+
+# This is to merge all _icons_rc_{}.py files together into one file named _icons_rc.py
+# Because I can't upload the entire _icons_rc.py to my repo, it's too big.
+# I can't use Git at the moment because I'm not near my PC and I just want get 
+# this shit down fast so I can move on to a new personal project.
+import os.path
+
+if os.path.isfile('src/_icons_rc.py'):
+    import src.merge as merge
+    input_prefix = "_icons_rc"  # Prefix of the input files
+    output_file = "_icons_rc.py"  # Name of the merged output file
+    merge.merge_files(input_prefix, output_file)
+else:
+    print(f"{output_file} has already existed.")
+    print("you can now delete these lines in main.py from line 9 to line 26.")
+    
+# You can also use `pyside6-rcc _icons_rc.qrc -o _icons_rc.py` instead. 
+# Remember to have PySide 6 installed and script directory included in the 
+# system environment path to be able to use PySide6 commands.
+
+
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self)
@@ -13,7 +35,8 @@ class MainWindow(QMainWindow):
         
         # self = QMainWindow class
         # self.ui = Ui_MainWindow / user interface class
-        #Use this if you only have one json file named "style.json" inside the root directory, "json" directory or "jsonstyles" folder.
+        # Use this if you only have one json file named "style.json" inside
+        # the root directory, "json" directory or "jsonstyles" folder.
         # loadJsonStyle(self, self.ui) 
         # Use this to specify your json file(s) path/name
         
